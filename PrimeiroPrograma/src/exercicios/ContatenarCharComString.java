0959ca8fe446f6f06f50f34fe54b36ac632119f2
Clayton Zambon <zambon@outlook.com.br>
2024-08-29 16:17:35 -0300

    Concatenar Char com String

---
+++ PrimeiroPrograma/src/exercicios/ContatenarCharComString.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class ContatenarCharComString {
+
+	public static void main(String[] args) {
+		
+		char inicial = 'Z';
+		String sobrenome = "ambon";
+		
+		System.out.println(inicial + sobrenome);
+
+	}
+
+}
