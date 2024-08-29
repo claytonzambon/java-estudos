cfc7e21d2440258fa9b9b17aea49e5de564c29fa
Clayton Zambon <zambon@outlook.com.br>
2024-08-29 16:09:03 -0300

    Exercicios Media duas notas decimais

---
+++ PrimeiroPrograma/src/exercicios/MediaDeDuasNotasDecimais.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class MediaDeDuasNotasDecimais {
+
+	public static void main(String[] args) {
+		
+		double notaDecimalUm = 8.3;
+		double notaDecimalDois = 7.6;
+		
+		double media = (notaDecimalUm + notaDecimalDois) / 2;
+		
+		System.out.println("Resultado: " + media);
+
+	}
+
+}
