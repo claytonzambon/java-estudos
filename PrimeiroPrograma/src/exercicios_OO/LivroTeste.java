0245c660b9992076d454cd6a024a49f1e448c997
Clayton Zambon <zambon@outlook.com.br>
2024-09-04 15:38:25 -0300

    Exercicios_OO - Classe livro

---
+++ PrimeiroPrograma/src/exercicios_OO/LivroTeste.java
@@ -0,0 +1,15 @@
+package exercicios_OO;
+
+public class LivroTeste {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		
+		livro.setTitulo("Procrastinação - Guia científico sobre como parar de procrastinar");
+		livro.setAutor("Lílian Soares");
+		
+		System.out.println(livro.exibirDetalhes());
+
+	}
+
+}
