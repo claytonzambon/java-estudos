0a51302c7c32ac9211568656f04388f4f754bda4
Clayton Zambon <zambon@outlook.com.br>
2024-09-04 16:48:30 -0300

    Curso OO - Evitando código duplicado - Polimorfismo

---
+++ PrimeiroPrograma/src/screenmatchOO/br/com/zambon/screnmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,28 @@
+package screenmatchOO.br.com.zambon.screnmatch.calculos;
+
+import screenmatchOO.br.com.zambon.screnmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+
+	private int tempoTotal = 0;
+
+	public int getTempoTotal() {
+		return this.tempoTotal;
+	}
+	
+	/*
+	public void inclui(Filme f) {
+		this.tempoTotal += f.getDuracaoEmMinutos();
+	}
+	
+	public void inclui(Serie s) {
+		this.tempoTotal += s.getDuracaoEmMinutos();
+	}
+	*/
+	
+	public void inclui(Titulo titulo) {
+		System.out.println("Adicionando duração em minutos de " + titulo);
+		this.tempoTotal += titulo.getDuracaoEmMinutos();
+	}
+	
+}
