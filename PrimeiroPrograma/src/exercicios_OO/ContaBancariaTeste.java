cd461d91a708489c9aec2cbb05658a69cef214af
Clayton Zambon <zambon@outlook.com.br>
2024-09-04 15:41:51 -0300

    Exercicios_OO - Classe ContaBancariaTeste

---
+++ PrimeiroPrograma/src/exercicios_OO/ContaBancariaTeste.java
@@ -0,0 +1,21 @@
+package exercicios_OO;
+
+public class ContaBancariaTeste {
+
+	public static void main(String[] args) {
+		
+		ContaBancaria conta = new ContaBancaria();
+		
+		conta.setNumeroConta(777);
+		conta.setSaldo(1000);
+		conta.titular = "Dona Florinda";
+		
+		System.out.println("Número da conta: " + conta.getNumeroConta());
+		System.out.println("Saldo: " + conta.getSaldo());
+		System.out.println("Titular: " + conta.titular);
+		
+		conta.setSaldo(1500);
+		System.out.println("Novo saldo: " + conta.getSaldo());
+	}
+
+}
