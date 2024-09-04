c5ccd265ac1710de5e7fdd0d978ae79754e3c685
Clayton Zambon <zambon@outlook.com.br>
2024-09-04 15:24:59 -0300

    Exercicios_OO - ContaBancaria, IdadePessoa, Produto

---
+++ PrimeiroPrograma/src/exercicios_OO/ContaBancaria.java
@@ -0,0 +1,24 @@
+package exercicios_OO;
+
+public class ContaBancaria {
+
+	private int numeroConta;
+	private double saldo;
+	public String titular;
+	
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
