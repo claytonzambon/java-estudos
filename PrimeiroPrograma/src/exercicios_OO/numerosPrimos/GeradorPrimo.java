f155bf8f79ef690c79c42f29c5b4beda2e510efb
Clayton Zambon <zambon@outlook.com.br>
2024-09-05 17:09:24 -0300

    Exercicios_oo - Numeros Primos

---
+++ PrimeiroPrograma/src/exercicios_OO/numerosPrimos/GeradorPrimo.java
@@ -0,0 +1,12 @@
+package exercicios_OO.numerosPrimos;
+
+public class GeradorPrimo extends NumerosPrimos{
+
+	public int gerarProximoPrimo(int ultimoPrimoConhecido) {
+		int proximoNumero = ultimoPrimoConhecido + 1;
+		while (!verificarPrimalidade(proximoNumero)) {
+			proximoNumero++;
+		}
+		return proximoNumero;
+	}
+}
