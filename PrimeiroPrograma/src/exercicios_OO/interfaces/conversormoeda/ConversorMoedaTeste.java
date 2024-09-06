7dc55646cae9f9b1cc6eea65562bd6be3636b380
Clayton Zambon <zambon@outlook.com.br>
2024-09-06 16:36:21 -0300

    Exercicios_OO - Conversor de Moeda

---
+++ PrimeiroPrograma/src/exercicios_OO/interfaces/conversormoeda/ConversorMoedaTeste.java
@@ -0,0 +1,11 @@
+package exercicios_OO.interfaces.conversormoeda;
+
+public class ConversorMoedaTeste {
+
+	public static void main(String[] args) {
+		ConversorMoeda conversor = new ConversorMoeda();
+		conversor.converterDolarParaReal(100);
+
+	}
+
+}
