fbf2db7283fabf35dc77c0a76a059b5e7f1b84f3
Clayton Zambon <zambon@outlook.com.br>
2024-09-07 18:05:44 -0300

    Metodos - Pessoa

---
+++ PrimeiroPrograma/src/exercicios_OO/metodos/pessoa/Pessoa.java
@@ -0,0 +1,8 @@
+package exercicios_OO.metodos.pessoa;
+
+public class Pessoa {
+
+	public String retornaOlaMundo() {
+		return "Olá, mundo!";
+	}
+}
---
+++ PrimeiroPrograma/src/exercicios_OO/metodos/pessoa/PessoaTeste.java
@@ -0,0 +1,11 @@
+package exercicios_OO.metodos.pessoa;
+
+public class PessoaTeste {
+
+	public static void main(String[] args) {
+		Pessoa pessoa = new Pessoa();
+		System.out.println(pessoa.retornaOlaMundo());
+
+	}
+
+}
