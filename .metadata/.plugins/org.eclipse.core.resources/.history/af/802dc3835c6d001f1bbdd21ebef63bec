b88053616cf8ac07ee0fbcd5379b88ce20845bbd
Clayton Zambon <zambon@outlook.com.br>
2024-09-07 18:03:03 -0300

    Exercicios_OO - Metodos - Calculadora

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/af/802dc3835c6d001f1bbdd21ebef63bec
@@ -0,0 +1,8 @@
+package exercicios_OO.calculadora;
+
+public class Calculadora {
+
+	public double retornaDobro(double numero) {
+		return numero * 2;
+	}
+}
---
+++ PrimeiroPrograma/src/exercicios_OO/metodos/calculadora/Calculadora.java
@@ -0,0 +1,8 @@
+package exercicios_OO.metodos.calculadora;
+
+public class Calculadora {
+
+	public double retornaDobro(double numero) {
+		return numero * 2;
+	}
+}
