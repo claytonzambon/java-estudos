27363f8b536288f2d856a9f51323e6ac1008971d
Clayton Zambon <zambon@outlook.com.br>
2024-09-07 17:46:59 -0300

    Exercicios_OO - Heranca - ContaBancaria

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/53/e07ec6355a6d001f1bbdd21ebef63bec
@@ -0,0 +1,11 @@
+package exercicios_OO.contabancaria;
+
+public class ContaCorrente extends ContaBancaria {
+
+	private double tarifaMensal = 2.00;
+	
+	public void cobrarTarifaMensal() {
+		saldo -= tarifaMensal;
+		System.out.println("Tarifa mensal de " + tarifaMensal + " cobrada. Saldo atual: " + saldo);
+	}
+}
---
+++ PrimeiroPrograma/src/exercicios_OO/heranca/contabancaria/ContaBancaria.java
@@ -0,0 +1,25 @@
+package exercicios_OO.heranca.contabancaria;
+
+public class ContaBancaria {
+
+	protected double saldo;
+	
+	public void depositar(double valor) {
+		saldo += valor;
+		System.out.println("Depósito de " + valor + " realizado. Saldo atual: " + saldo);
+	}
+	
+	public void sacar(double valor) {
+		if (valor <= saldo) {
+			saldo -= valor;
+			System.out.println("Saque de " + valor + " realizado. Saldo atual: " + saldo);
+		} else {
+			System.out.println("Saldo insuficiente para saque.");
+		}
+		
+	}
+	
+	public void consultarSaldo() {
+		System.out.println("Saldo atual: " + saldo);
+	}
+}
