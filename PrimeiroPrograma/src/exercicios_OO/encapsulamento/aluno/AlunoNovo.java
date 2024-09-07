820f0847244cd876e628100e3b099ceb48c3494a
Clayton Zambon <zambon@outlook.com.br>
2024-09-07 17:54:51 -0300

    Exercicios_OO - Encapsulamento - Aluno

---
+++ PrimeiroPrograma/src/exercicios_OO/encapsulamento/aluno/AlunoNovo.java
@@ -0,0 +1,37 @@
+package exercicios_OO.encapsulamento.aluno;
+
+public class AlunoNovo {
+
+	private String nome;
+	private double notas;
+	private int qtdNotas;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getNotas() {
+		return notas;
+	}
+	public void setNotas(double notas) {
+		this.notas += notas;
+		setQtdNotas(+1);
+	}
+	
+	public int getQtdNotas() {
+		return qtdNotas;
+	}
+	public void setQtdNotas(int qtdNotas) {
+		this.qtdNotas += qtdNotas;
+	}
+	public double calcularMedia() {
+		return getNotas() / qtdNotas;
+	}
+	
+	public String toString() {
+		return String.format("Nome do aluno: %s \nMedia do aluno: %.2f", getNome(), calcularMedia());
+	}
+}
---
+++ PrimeiroPrograma/src/exercicios_OO/encapsulamento/aluno/AlunoNovoTeste.java
@@ -0,0 +1,17 @@
+package exercicios_OO.encapsulamento.aluno;
+
+public class AlunoNovoTeste {
+
+	public static void main(String[] args) {
+		AlunoNovo alunoNovo = new AlunoNovo();
+		
+		alunoNovo.setNome("Nhonho");
+		alunoNovo.setNotas(7);
+		alunoNovo.setNotas(9);
+		alunoNovo.setNotas(9);
+		
+		System.out.println(alunoNovo);
+
+	}
+
+}
