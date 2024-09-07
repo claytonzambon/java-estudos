a9b4628645761551fda6258d637fd8842fb2c10d
Clayton Zambon <zambon@outlook.com.br>
2024-09-07 17:58:41 -0300

    Exercicios_OO - Encapsulamento - Livro

---
+++ PrimeiroPrograma/src/exercicios_OO/encapsulamento/livro/Livro.java
@@ -0,0 +1,25 @@
+package exercicios_OO.encapsulamento.livro;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public String exibirDetalhes() {
+		return String.format("Título do Livro: %s \nAutor do Livro: %s", getTitulo(), getAutor());
+	}
+	
+}
