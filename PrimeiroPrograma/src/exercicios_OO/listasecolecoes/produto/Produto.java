a70b85eb5f8e6b150d35699eaec6068a4ec497ec
Clayton Zambon <zambon@outlook.com.br>
2024-09-08 20:16:47 -0300

    Exercicios_Lista_e_Colecoes - Lista de Produtos

---
+++ PrimeiroPrograma/src/exercicios_OO/listasecolecoes/produto/Produto.java
@@ -0,0 +1,26 @@
+package exercicios_OO.listasecolecoes.produto;
+
+public class Produto {
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
