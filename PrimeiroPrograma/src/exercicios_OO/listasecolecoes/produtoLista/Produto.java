a2369839b3987687286e8d5ae3ce795065c909b0
Clayton Zambon <zambon@outlook.com.br>
2024-09-09 11:17:04 -0300

    Exercicios_Lista_e_Colecoes - Classe ProdutoLista

---
+++ PrimeiroPrograma/src/exercicios_OO/listasecolecoes/produtoLista/Produto.java
@@ -0,0 +1,33 @@
+package exercicios_OO.listasecolecoes.produtoLista;
+
+public class Produto {
+	private String nome;
+	private double preco;
+	
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto: " + nome + " (R$ " + preco + ")";
+	}
+	
+}
