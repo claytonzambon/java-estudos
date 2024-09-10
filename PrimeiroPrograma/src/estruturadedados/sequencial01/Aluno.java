0c907bf79edad06324f2facc8bd7c2af94caafae
Clayton Zambon <zambon@outlook.com.br>
2024-09-10 14:53:17 -0300

    Java - Estrutura de Dados - Lista Sequencial

---
+++ PrimeiroPrograma/src/estruturadedados/sequencial01/Aluno.java
@@ -0,0 +1,39 @@
+package estruturadedados.sequencial01;
+
+import java.util.Objects;
+
+public class Aluno {
+	private String nome;
+	
+	public Aluno(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return nome;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
+	
+	
+}
