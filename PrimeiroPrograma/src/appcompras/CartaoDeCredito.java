505c61c74099091f77fbd40ea5f47ae50cbb6343
Clayton Zambon <zambon@outlook.com.br>
2024-09-10 11:03:47 -0300

    Exercicios_Lista_e_Colecoes - App de Compras

---
+++ PrimeiroPrograma/src/appcompras/CartaoDeCredito.java
@@ -0,0 +1,39 @@
+package appcompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+	private double limite;
+	private double saldo;
+	private List<Compra> compras;
+	
+	public CartaoDeCredito(double limite) {
+		this.limite = limite;
+		this.saldo = limite;
+		this.compras = new ArrayList<>();
+	}
+	
+	public boolean adicionaCompra(Compra compra) {
+		if (saldo > compra.getValor()) {
+			saldo -= compra.getValor();
+			compras.add(compra);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public List<Compra> getCompras() {
+		return compras;
+	}
+	
+}
---
+++ PrimeiroPrograma/src/appcompras/Compra.java
@@ -0,0 +1,24 @@
+package appcompras;
+
+public class Compra {
+	private String descricao;
+	private double valor;
+	
+	public Compra(String descricao, double valor) {
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Compra [descricao: " + descricao + ", valor: R$ " + valor + "]";
+	}
+}
