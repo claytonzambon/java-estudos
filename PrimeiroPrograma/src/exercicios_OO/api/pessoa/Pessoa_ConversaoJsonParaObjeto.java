1052af0306330f558a77eed288ad20a8f4577004
Clayton Zambon <zambon@outlook.com.br>
2024-09-13 16:24:22 -0300

    Exercicio_API - Classe Pessoa - Parte 2

--- PrimeiroPrograma/src/exercicios_OO/api/pessoa/Pessoa_ConversaoJsonParaObjeto.java
+++ PrimeiroPrograma/src/exercicios_OO/api/pessoa/Pessoa_ConversaoJsonParaObjeto.java
@@ -1,6 +1,7 @@
 package exercicios_OO.api.pessoa;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Pessoa_ConversaoJsonParaObjeto {
 
@@ -8,7 +9,8 @@ public class Pessoa_ConversaoJsonParaObjeto {
 		
 		String jsonPessoa = "{\"nome\":\"Mercurio\",\"idade\":41,\"cidade\":\"Lages\"}";
 		
-		Gson gson = new Gson();
+		//Gson gson = new Gson();
+		Gson gson = new GsonBuilder().setLenient().create();
 		Pessoa pessoa = gson.fromJson(jsonPessoa, Pessoa.class);
 		
 		System.out.println("Objeto Pessoa: " + pessoa);
